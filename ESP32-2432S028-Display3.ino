43c265b6d15b2373c2b6a9569ec4b8b55ca5f81b
OttoMeister <OttoMeister@users.noreply.github.com>
2023-01-08 08:59:19 -0500

    Update ESP32-2432S028-Display3.ino

--- ESP32-2432S028-Display3.ino
+++ ESP32-2432S028-Display3.ino
@@ -12,34 +12,6 @@ alignment: steer towards the average heading of local flockmates
 cohesion: steer to move towards the average position (center of mass) of local flockmates
 */
 
-/*
-Copy this in ~/Arduino/libraries/TFT_eSPI/User_Setup.h
-#define ILI9341_2_DRIVER    
-#define TFT_WIDTH  240
-#define TFT_HEIGHT 320
-#define TFT_BL   21           
-#define TFT_BACKLIGHT_ON HIGH  
-#define TFT_MOSI 13 
-#define TFT_SCLK 14
-#define TFT_CS   15  
-#define TFT_DC   2  
-#define TFT_RST  12  
-#define TFT_BL   21 
-#define TOUCH_CS 33  
-#define LOAD_GLCD   
-#define LOAD_FONT2  
-#define LOAD_FONT4  
-#define LOAD_FONT6  
-#define LOAD_FONT7  
-#define LOAD_FONT8  
-//#define LOAD_FONT8N 
-#define LOAD_GFXFF  
-#define SMOOTH_FONT
-#define SPI_FREQUENCY  55000000 
-#define SPI_READ_FREQUENCY  20000000
-#define SPI_TOUCH_FREQUENCY  2500000
-*/
-
 #define WITH_TIME
 #include <TFT_eSPI.h>
 #include <SPI.h>
